b78fa0ffa12a6d7463910d2d17937c819efd3074
GodKong2431 <kizna10231@gmail.com>
2025-09-11 21:54:44 +0900

    [level 0] Title: n의 배수, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int num, int n) 
+    {
+        if (num % n == 0)
+        {
+            return 1;
+        }
+        return 0;
+    }
+}
