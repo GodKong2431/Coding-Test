5bcd69b592975b76bb62c28df49b881bcda2abf3
GodKong2431 <kizna10231@gmail.com>
2025-09-12 18:47:53 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.25 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = a+b+c;
+        if (a == b || a == c || b == c)
+        {
+            answer *= a*a+b*b+c*c;
+        }
+        if (a == b && a == c)
+        {
+            answer *= a*a*a+b*b*b+c*c*c;
+        }
+        return answer;
+    }
+}
