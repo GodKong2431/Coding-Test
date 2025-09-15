d240279f0b827868a3447c3220aad19fcb4b4eca
GodKong2431 <kizna10231@gmail.com>
2025-09-15 19:15:01 +0900

    [level 0] Title: 수 조작하기 2, Time: 4959.03 ms, Memory: 51.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public string solution(int[] numLog) {
+        string answer = "";
+        for(int i = 0; i < numLog.Length-1; i++)
+        {
+            if (numLog[i]+1 == numLog[i+1]) answer+='w';
+            else if(numLog[i]-10 == numLog[i+1]) answer+='a';
+            else if(numLog[i]+10 == numLog[i+1]) answer+='d';
+            else if(numLog[i]-1 == numLog[i+1]) answer += 's';
+        }
+        return answer;
+    }
+}
