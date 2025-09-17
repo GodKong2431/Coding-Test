e76d7a89f5975d7c17bbc81827b528acd258220d
GodKong2431 <kizna10231@gmail.com>
2025-09-17 18:53:37 +0900

    [level 0] Title: 배열 만들기 1, Time: 2.84 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[n/k];
+        int count = 0;
+        for (int i = 1; i <= n;i++)
+        {
+            if(i % k == 0)
+            {
+                answer[count] = i;
+                count++;
+            }
+        }
+        return answer;
+    }
+}
