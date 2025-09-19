98e5304373bbfa96db258cb4b2083e19b1cf5ecf
GodKong2431 <kizna10231@gmail.com>
2025-09-19 19:36:38 +0900

    [level 0] Title: 양꼬치, Time: 0.24 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int A = n * 12000;
+        int B = k * 2000;
+        int S = n / 10;
+            
+        return (n*12000) + ((k-(n/10))*2000);
+    }
+}
