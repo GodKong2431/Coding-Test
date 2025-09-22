92698cbe0138f79ab6539eb5e626883b86062388
GodKong2431 <kizna10231@gmail.com>
2025-09-22 19:53:34 +0900

    [level 0] Title: 세균 증식, Time: 0.18 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int t) {
+        int answer = n;
+        for (int i = t; i > 0; i--)
+        {
+            answer *= 2;
+        }
+        return answer;
+    }
+}
