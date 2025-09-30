a2557dfb91df15516e7d361064cc69770b260398
GodKong2431 <kizna10231@gmail.com>
2025-09-30 09:39:59 +0900

    [level 0] Title: 순서쌍의 개수, Time: 3.53 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            if(((n / i) * i) == n)
+                answer++;
+        }
+        return answer;
+    }
+}
