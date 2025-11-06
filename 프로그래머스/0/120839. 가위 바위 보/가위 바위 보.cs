b5cfc058549f46eabb6eea1f89b055fa2a54a8c0
GodKong2431 <kizna10231@gmail.com>
2025-11-06 23:47:14 +0900

    [level 0] Title: 가위 바위 보, Time: 0.27 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution {
+    public string solution(string rsp) {
+        string answer = "";
+        
+        foreach(char c in rsp)
+        {
+            if(c == '0')
+            {
+                answer += "5";
+            }
+            else if(c == '2')
+            {
+                answer += "0";
+            }
+            else
+            {
+                answer += "2";
+            }
+        }
+        
+        return answer;
+    }
+}
