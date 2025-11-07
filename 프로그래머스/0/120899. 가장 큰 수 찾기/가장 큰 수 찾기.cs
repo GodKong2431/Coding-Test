5382c2ad6f2df6737c2983df1db6c0459001416e
GodKong2431 <kizna10231@gmail.com>
2025-11-07 16:36:12 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        for(int i = 0; i < array.Length; i++)
+        {
+            if(answer[0] < array[i])
+            {
+                answer[0] = array[i];
+                answer[1] = i;
+            }
+        }
+        return answer;
+    }
+}
