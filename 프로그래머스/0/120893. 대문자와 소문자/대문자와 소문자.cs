645d39010fcdbc0783ba32622603e4d92a1eafa5
GodKong2431 <kizna10231@gmail.com>
2025-11-07 16:03:23 +0900

    [level 0] Title: 대문자와 소문자, Time: 1.25 ms, Memory: 32.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string) {
+        string answer = "";
+        foreach(char c in my_string)
+        {
+            if(Char.IsUpper(c))
+            {
+                answer += Char.ToLower(c);
+            }
+            else
+            {
+                answer += Char.ToUpper(c);
+            }
+        }
+        return answer;
+    }
+}
