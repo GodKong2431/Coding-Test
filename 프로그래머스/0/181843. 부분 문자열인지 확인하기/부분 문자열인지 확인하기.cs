be64e64bd48e0c70d5010fd95fe6f19b188ffe49
GodKong2431 <kizna10231@gmail.com>
2025-11-10 22:56:54 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.28 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(string my_string, string target) {
+        int answer = 0;
+        
+        if(my_string.Contains(target))
+        {
+            return 1;
+        }
+        return answer;
+    }
+}
