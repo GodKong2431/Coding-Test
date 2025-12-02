8071f33a2d0b05645e519a682a48cc576b559c4a
GodKong2431 <kizna10231@gmail.com>
2025-12-02 16:29:24 +0900

    [level 0] Title: 배열 만들기 5, Time: 4.92 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.cs
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.cs
@@ -1,24 +1,10 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
-    public int[] solution(string[] intStrs, int k, int s, int l) {
-        List<int> answer = new List<int> ();
-        foreach(string str in intStrs)
-        {
-            string temp = "";
-            for(int i = s; i < s+l; i++)
-            {
-                temp += str[i];
-            }
-            
-            int tempNum = int.Parse(temp);
-            
-            if(tempNum > k)
-            {
-                answer.Add(tempNum);
-            }
-        }
-        return answer.ToArray();
+    public int[] solution(string[] intStrs, int k, int s, int l) 
+    {
+        return intStrs.Select(e => int.Parse(e.Substring(s, l)))
+                      .Where(w => w > k).ToArray();
     }
 }
