dfbc11e2cc69fcf4bbdf072dc6c43f4c0e9a0a1a
GodKong2431 <kizna10231@gmail.com>
2025-12-04 16:32:54 +0900

    [level 0] Title: 모스부호 (1), Time: 1.92 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public string solution(string letter) {
+        string answer = "";
+        string[] splitArr = letter.Split(' ');
+        Dictionary<string, char> morse= new Dictionary<string, char>
+        { 
+            {".-", 'a'},
+            {"-...",'b'},
+            {"-.-.",'c'},
+            {"-..",'d'},
+            {".",'e'},
+            {"..-.",'f'},
+            {"--.",'g'},
+            {"....",'h'},
+            {"..",'i'},
+            {".---",'j'},
+            {"-.-",'k'},
+            {".-..",'l'},
+            {"--",'m'},
+            {"-.",'n'},
+            {"---",'o'},
+            {".--.",'p'},
+            {"--.-",'q'},
+            {".-.",'r'},
+            {"...",'s'},
+            {"-",'t'},
+            {"..-",'u'},
+            {"...-",'v'},
+            {".--",'w'},
+            {"-..-",'x'},
+            {"-.--",'y'},
+            {"--..",'z'}
+        };
+        
+        foreach(string str in splitArr)
+        {
+            answer += morse[str];
+        }
+        
+        return answer;
+    }
+}
