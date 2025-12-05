1ccab08d84434f4fbfbea480be1fd4ebedc8cc6d
GodKong2431 <kizna10231@gmail.com>
2025-12-05 12:08:05 +0900

    [level 0] Title: 문자열 묶기, Time: 12.34 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.cs
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.cs
@@ -3,15 +3,7 @@ using System.Linq;
 
 public class Solution {
     public int solution(string[] strArr) {
-        int answer = 0;
         
-        int[] counts = new int[31];
-        foreach(string str in strArr)
-        {
-            counts[str.Length]++;
-        }
-        
-        answer = counts.Max();
-        return answer;
+        return strArr.GroupBy(s => s.Length).Max(g => g.Count());;
     }
 }
