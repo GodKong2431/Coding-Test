58b1ae69e68d08b0e541b69e45090b83a4c887d2
GodKong2431 <kizna10231@gmail.com>
2025-12-30 18:50:03 +0900

    [level 0] Title: 최빈값 구하기, Time: 2.25 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cs
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cs
@@ -1,8 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 public class Solution {
     public int solution(int[] array) {
-        int answer = 0;
-        return answer;
+        Dictionary<int, int> counts = new Dictionary<int, int>();
+        foreach (int num in array) {
+            if (counts.ContainsKey(num)) {
+                counts[num]++;
+            } else {
+                counts[num] = 1;
+            }
+        }
+
+        int maxCount = 0;
+        int same = -1;
+        bool sameCheck = false;
+
+        foreach (var pair in counts) {
+            if (pair.Value > maxCount) {
+                maxCount = pair.Value;
+                same = pair.Key;
+                sameCheck = false;
+            } else if (pair.Value == maxCount) {
+                sameCheck = true;
+            }
+        }
+
+        if (sameCheck) {
+            return -1;
+        } else {
+            return same;
+        }
     }
 }
